23a33f7c58698835ca480453347715e4fd08ba93
Aaron Harmon <aaronharmon@Aarons-MBP.fios-router.home>
2019-06-16 11:09:19 -0400

    Asks user to input app name.

--- setup_flask.sh
+++ setup_flask.sh
@@ -2,6 +2,7 @@
 
 #This script starts up a virtual environment, sets your flask variables and starts your server.
 #. venv/bin/activateexport 
-export FLASK_APP=app.py
+read -p "What's the app name? " appName
+export FLASK_APP=$appName
 export FLASK_ENV=development
 flask run
