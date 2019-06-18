9337cb0ea4af85ad439c654eafdf666cd590a85a
Aaron Harmon <aaronharmon@Aarons-MBP.fios-router.home>
2019-06-17 21:05:31 -0400

    Added VENV activation.

--- setup_flask.sh
+++ setup_flask.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #This script starts up a virtual environment, sets your flask variables and starts your server.
-#. venv/bin/activateexport 
+source venv/bin/activate
 read -p "What's the app name? " appName
 export FLASK_APP=$appName
 export FLASK_ENV=development
